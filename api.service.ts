37bbcd7c4bfe8969652f3136024e9ca8a03e6228
vinothr196 <53854249+vinothr196@users.noreply.github.com>
2024-10-28 11:09:46 +0000

    Create api.service.ts
    implement the login function

---
+++ api.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'https://connect.paj-gps.de/api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password });
+  }
+
+  getDevices(token: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/device`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+  }
+
+  getLastTrackingData(deviceId: string, token: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/trackerdata/${deviceId}/last_points?lastPoints=50`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+  }
+}
